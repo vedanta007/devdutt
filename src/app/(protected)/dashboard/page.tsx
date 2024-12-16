ce7edf9102ecb88e73aa9ef8091cbe81498a8c14
vedanta007 <vedantasingh007@gmail.com>
2024-12-16 12:43:45 +0530

    Add octokit dependency and enhance dashboard with GitHub link and layout improvements

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import useProject from "@/hooks/use-project";
-import { Github } from "lucide-react";
+import { ExternalLink, Github } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 const DashboardPage = () => {
@@ -8,13 +9,36 @@ const DashboardPage = () => {
     return (
         <div>
             <div className="flex items-center justify-between flex-wrap gap-y-4">
+                {/* Github Link Banner */}
                 <div className="w-fit rounded-md bg-primary px-4 py-3">
-                    <Github className="size-5 text-white" />
-                    <div className="ml-2">
-                        <p className="text-sm font-medium text-white"></p>
+                    <div className="flex items-center">
+                        <Github className="size-5 text-white" />
+                        <div className="ml-2">
+                            <p className="text-sm font-medium text-white">
+                                This project is connected to {''}
+                                <Link href={project?.githubUrl ?? ''} className="inline-flex items-center text-white/80 hover:underline">
+                                {project?.githubUrl}
+                                <ExternalLink className="ml-1 size-4"/>
+                                </Link>
+                            </p>
+                        </div>
                     </div>
                 </div>
+                <div className="h-4"></div>
+                <div className="flex items-center gap-4">
+                    TeamMembers
+                    Invite Button
+                    Archive Button
+                </div>
             </div>
+            <div className="mt-4">
+                <div className="grid grid-cols-1 gap-4 sm:grid-cols-5">
+                    Ask a Question Card
+                    Meeting Card
+                </div>
+            </div>
+            <div className="mt-8"></div>
+            Commit log
         </div>
     );
 }
