3b0be49e76a87fde785207bebfa3a9c55898c6cb
vedanta007 <vedantasingh007@gmail.com>
2024-12-18 14:00:40 +0530

    Add dialog for question submission in AskQuestionCard component

--- src/app/(protected)/dashboard/ask-question-card.tsx
+++ src/app/(protected)/dashboard/ask-question-card.tsx
@@ -1,19 +1,31 @@
 'use client'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog'
 import { Textarea } from '@/components/ui/textarea'
 import useProject from '@/hooks/use-project'
+import Image from 'next/image'
 import React from 'react'
 
 const AskQuestionCard = () => {
     const { project } = useProject()
+    const [open, setOpen] = React.useState(false)
     const [question, setQuestion] = React.useState('')
     const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
-        window.alert(question)
+        setOpen(true)
     }
     return (
         <>
+            <Dialog open={open} onOpenChange={setOpen}>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>
+                            <Image src="/logo.svg" alt='Logo' width={40} height={40} />
+                        </DialogTitle>
+                    </DialogHeader>
+                </DialogContent>
+            </Dialog>
             <Card className='relative col-span-3'>
                 <CardHeader>
                     <CardTitle>Ask a Question</CardTitle>
