81ecef06956b856b6ed242f0f7cd51bf801a3dbc
vedanta007 <vedantasingh007@gmail.com>
2024-12-19 20:25:51 +0530

    Add Firebase configuration to environment variables and update firebase.ts to use them

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,20 +2,19 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getDownloadURL, getStorage, ref, uploadBytes, uploadBytesResumable } from "firebase/storage";
-import { error } from "console";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAXj1a0cLiJGdFgsOGs0KzRrEhWpYzvK-4",
-  authDomain: "devdutt-b685c.firebaseapp.com",
-  projectId: "devdutt-b685c",
-  storageBucket: "devdutt-b685c.firebasestorage.app",
-  messagingSenderId: "482715356867",
-  appId: "1:482715356867:web:65fffabcea626748af5c76",
-  measurementId: "G-W9ZYF2ERT5"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
