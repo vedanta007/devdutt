0170092ea14c9d643164a4177bb74a694f73eba3
vedanta007 <vedantasingh007@gmail.com>
2024-12-20 14:29:25 +0530

    Add ArchiveButton component and functionality to archive projects

--- src/server/api/routers/project.ts
+++ src/server/api/routers/project.ts
@@ -127,4 +127,16 @@ export const projectRouter = createTRPCRouter({
                 issues: true
             }})
     }),
+    archiveProject: protectedProcedure.input(z.object({
+        projectId: z.string()
+    })).mutation(async({ctx, input})=>{
+        return await ctx.db.project.update({
+            where: {
+                id: input.projectId
+            },
+            data: {
+                deletedAt: new Date()
+            }
+        })
+    }),
 })
